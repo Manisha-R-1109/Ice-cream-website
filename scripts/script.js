af5742a0e8beb5fd5b996e56f1a8dc70bbdb4a6b
Manisha-R-1109 <manisha.rangasamy@gmail.com>
2026-05-21 14:11:08 +0530

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,12 @@
-var slideIndex = 0;
-showSlides();
+fetch("http://13.232.46.251:8000/products")
+  .then(response => response.json())
+  .then(data => {
+      console.log(data);
 
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) { slideIndex = 1 }
-  slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
+      data.forEach(item => {
+          console.log(item.name);
+      });
+  })
+  .catch(error => {
+      console.log(error);
+  });
