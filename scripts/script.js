23f5bf37afd428eb6b78640b75154ffb94258cd3
Manisha-R-1109 <manisha.rangasamy@gmail.com>
2026-05-21 17:34:16 +0530

    script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,27 @@
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slideIndex++;
+
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+
+  setTimeout(showSlides, 2000);
+}
+
+
+// Backend API Connection
 fetch("http://13.232.46.251:8000/products")
   .then(response => response.json())
   .then(data => {
